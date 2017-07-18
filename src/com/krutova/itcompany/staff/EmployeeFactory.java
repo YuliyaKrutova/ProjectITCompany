ba037ab604db617b509b5f027c1803603287ca0e
Julia <Julia@192.168.56.1>
2017-07-18 21:49:54 +0300

    ...

--- src/com/krutova/itcompany/staff/EmployeeFactory.java
+++ src/com/krutova/itcompany/staff/EmployeeFactory.java
@@ -11,13 +11,17 @@ public class EmployeeFactory {
 	
 	public Employee newEmployee(String[] str) throws CompanyException {
 		
-		if (DataSourceType.DEVELOPER.name().equals(str[0])){
-		return new Developer(str[1], str[2], Integer.parseInt(str[3]), ContractType.valueOf(str[4]), DevTitle.valueOf(str[5]), Integer.parseInt(str[6]));
-		} else if (DataSourceType.MANAGER.name().equals(str[0])){
-			return new  Manager(str[1], str[2], Integer.parseInt(str[3]), ContractType.valueOf(str[4]),ManagerType.valueOf(str[5]));
-		} else{
+		try{
+		switch (DataSourceType.valueOf(str[0])){
+		case DEVELOPER: return new Developer(str[1], str[2], Integer.parseInt(str[3]), ContractType.valueOf(str[4]), DevTitle.valueOf(str[5]), Integer.parseInt(str[6]));
+		case MANAGER: return new Manager(str[1], str[2], Integer.parseInt(str[3]), ContractType.valueOf(str[4]),ManagerType.valueOf(str[5]));
+		default: return null; //никогда сюда не попадет
+		}
+		}
+		catch (IllegalArgumentException e){
 			throw new CompanyException("Не существующий тип Employee в файле1");
 		}
+
 	}
 
 }
